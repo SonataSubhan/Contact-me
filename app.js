219fc3d0058992684e32193d2abebf39c929b88f
Subhan Huseynov <johndoe@email.com>
2024-09-30 15:04:10 +0400

    v03

---
+++ app.js
@@ -0,0 +1,34 @@
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const btn3 = document.getElementById('btn3');
+const display1 = document.getElementById('display1');
+const display2 = document.getElementById('display2');
+const display3 = document.getElementById('display3');
+
+btn1.onclick = function one() {
+    display1.style.display = "grid";
+    display2.style.display = "none";
+    display3.style.display = "none";
+    btn1.style.backgroundColor = "white";
+    btn2.style.backgroundColor = "#6e4c41";
+    btn3.style.backgroundColor = "#6e4c41";
+
+}
+btn2.onclick = function two() {
+    display1.style.display = "none";
+    display2.style.display = "grid";
+    display3.style.display = "none";
+    btn1.style.backgroundColor = "#6e4c41";
+    btn2.style.backgroundColor = "white";
+    btn3.style.backgroundColor = "#6e4c41";
+}
+
+
+btn3.onclick = function three() {
+    display1.style.display = "none";
+    display2.style.display = "none";
+    display3.style.display = "flex";
+    btn1.style.backgroundColor = "#6e4c41";
+    btn2.style.backgroundColor = "#6e4c41";
+    btn3.style.backgroundColor = "white";
+}
